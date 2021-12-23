9d76286e01fad40575e9c4ce8641b2c032b47e02
Taoge <Taoge@hahaha.com>
2021-12-23 20:54:04 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git! 这是idea测试git！");
+        System.out.println("hello git2! 这是idea测试git2！");
     }
 }
