e0b7bd17ae5704889f0a0a70bf48c757dd69419d
Taoge <Taoge@hahaha.com>
2021-12-23 21:01:52 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git! 这是idea测试git！");
         System.out.println("hello git2! 这是idea测试git2！");
         System.out.println("hello git3! 这是idea测试git3！");
+        System.out.println("hello git4! 这是idea测试git4！");
     }
 }
