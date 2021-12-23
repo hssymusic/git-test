11f902c2f14a4d9da9e55075d52cfe25ec395860
Taoge <Taoge@hahaha.com>
2021-12-23 21:39:20 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git4! 这是idea测试git4！");
         System.out.println("master test!");
         System.out.println("hot-fix test!");
+        System.out.println("push test!");
     }
 }
