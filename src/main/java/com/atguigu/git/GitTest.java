588fb111728669c287f0d5b60fc3afc3bc6d7b26
Taoge <Taoge@hahaha.com>
2021-12-23 20:55:54 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git! 这是idea测试git！");
         System.out.println("hello git2! 这是idea测试git2！");
+        System.out.println("hello git3! 这是idea测试git3！");
     }
 }
